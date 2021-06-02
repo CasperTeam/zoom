799cb2c48b532b0dfee87dbd50992ebf90ff11e4
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-06-02 15:02:55 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,13 @@
+class Translation(object):
+    START_TEXT = """Hi!
+I cant help you beacuse you are not authorized 
+contact @a_l_satya to use me or he will provide another bot
+remember only he can do this dont spam me if you spam i will not give you reply
+
+So Stop here my friend.. """
+    AFTER_RECVD_CODE_TEXT = """I said just stop here dont spam me cotact @a_l_satya to use me"""
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    ERRED_PAGE = "something wrongings."
+    CANCELLED_MESG = "I said just stop here dont spam me cotact @a_l_satya to use me"
+    IN_VALID_CODE_PVDED = "I said just stop here dont spam me cotact @a_l_satya to use me"
+    IN_VALID_PHNO_PVDED = "I said just stop here dont spam me cotact @a_l_satya to use me"
